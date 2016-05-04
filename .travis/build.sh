5bb9a5388ce2019c0954c96b5c7c6b39159a6942
Michael Clausen <encodeering@gmail.com>
2016-05-04 21:28:24 +0200

    updates postgres 9.4 patch
    * using postgres> git diff --minimal > ../.patch/9.4/Dockerfile.patch
    
    NOTE
    
    * rejected patch had been applied manually again, but regenerated with git this time
    ** patch command would fail if not called with -p1

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,7 +8,7 @@ TAGSPECIFIER="$VERSION"
 docker pull   "$REPOSITORY/debian-$ARCH:jessie"
 docker tag -f "$REPOSITORY/debian-$ARCH:jessie" "debian:jessie"
 
-patch -p0 --no-backup-if-mismatch --directory=$PROJECT < .patch/$VERSION/Dockerfile.patch
+patch -p1 --no-backup-if-mismatch --directory=$PROJECT < .patch/$VERSION/Dockerfile.patch
 
 docker build -t "$TAG:$TAGSPECIFIER" --build-arg PG_VERSION="$VERSIONPIN" "$PROJECT/$VERSION"
 
