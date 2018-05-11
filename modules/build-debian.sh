5a570749fb07f7fe5a86eea3cababbddbb350f55
Michael Clausen <encodeering@gmail.com>
2018-05-11 10:01:43 +0200

    builds postgres 9.6 instead of 9.4
    * official images switched base image from jessie to stretch, and it might be better to increase the version instead of patching the base image
    
    NOTE
    
    * alpine 3.7 patch can be removed as 9.6 already uses 3.7
    
    * travis seems to have problems with keyserver (ipv6 pool)

--- modules/build-debian.sh
+++ modules/build-debian.sh
@@ -5,7 +5,7 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/debian-$ARCH:jessie" "debian:jessie"
+docker-pull "$REPOSITORY/debian-$ARCH:stretch" "debian:stretch-slim"
 
 docker-patch patch "$PROJECT"
 
