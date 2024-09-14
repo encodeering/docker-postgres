ea6ed61b41a18d1eecde66c19759742832968faf
Michael Clausen <encodeering@gmail.com>
2024-09-14 08:07:20 +0200

    updates postgres to the latest version
    * https://github.com/docker-library/postgres/commit/eaa1c35769621a6bb1e499073a5812ba478c7688

--- modules/build-alpine.sh
+++ modules/build-alpine.sh
@@ -5,8 +5,6 @@ set -e
 import com.encodeering.ci.config
 import com.encodeering.ci.docker
 
-docker-pull "$REPOSITORY/alpine-$ARCH:3.11" "alpine:3.11"
+docker-pull "$REPOSITORY/alpine-$ARCH:3.20" "alpine:3.20"
 
-docker-patch patch "$PROJECT"
-
-docker-build "$PROJECT/$VERSION/alpine"
+docker-build "$PROJECT/$VERSION/alpine3.20"
